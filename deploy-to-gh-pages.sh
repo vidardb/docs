7869b582304c1a6217f9bbea49565e02e92a240d
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-02-16 17:25:32 -0500

    Try to make mkdocs-deploy work.

---
+++ deploy-to-gh-pages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+git config user.name "no one cares"
+git config user.email "noonecares@gmail.com"
+git remote add gh-token "https://${GH_TOKEN}@github.com/vidardb/docs.git";
+git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+mkdocs gh-deploy -v --clean --remote-name gh-token;
+
