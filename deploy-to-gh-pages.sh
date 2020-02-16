98529f5cc03c7ca620a740a93aaf426925ad76b3
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-02-16 17:46:03 -0500

    Use my email and username

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -4,8 +4,8 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
-git config user.name "no one cares"
-git config user.email "noonecares@gmail.com"
+git config user.name "xingweitian"
+git config user.email "xingweitian@gmail.com"
 git remote add gh-token "https://${GH_TOKEN}@github.com/vidardb/docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy -v --clean --remote-name gh-token;
