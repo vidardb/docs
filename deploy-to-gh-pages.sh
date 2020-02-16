e968c453753d04ae630612c90206a8a5b36a0289
xingweitian <13183370+xingweitian@users.noreply.github.com>
2020-02-16 18:16:23 -0500

    Update username and email.

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -4,8 +4,8 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
 
-git config user.name "xingweitian"
-git config user.email "xingweitian@gmail.com"
+git config user.name "vidarDB"
+git config user.email "travis.vidardb@gmail.com"
 git remote add gh-token "https://${GITHUB_TOKEN}@github.com/vidardb/docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy -v --clean --remote-name gh-token;
