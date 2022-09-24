c5fa2ed6f5fae9a536470671a0a0dee0dc0aa74e
Weitian Xing <xingweitian@gmail.com>
2022-09-24 13:17:43 -0400

    add ignore-version arg.

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -6,4 +6,4 @@ git config user.name "VidarDB"
 git config user.email "travis.vidardb@gmail.com"
 git remote add gh-token "https://${ACCESS_TOKEN}@github.com/vidardb/docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
-mkdocs gh-deploy -v --clean --remote-name gh-token;
+mkdocs gh-deploy -v --clean --remote-name --ignore-version gh-token;
