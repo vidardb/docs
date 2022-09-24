6e1b40c88beae0407fe6747baf78f6daa1ca0918
Weitian Xing <xingweitian@gmail.com>
2022-09-24 13:15:19 -0400

    Use github action.

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
-
 git config user.name "VidarDB"
 git config user.email "travis.vidardb@gmail.com"
-git remote add gh-token "https://${GITHUB_TOKEN}@github.com/vidardb/docs.git";
+git remote add gh-token "https://${ACCESS_TOKEN}@github.com/vidardb/docs.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 mkdocs gh-deploy -v --clean --remote-name gh-token;
-
